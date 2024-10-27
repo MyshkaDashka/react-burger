9f57219141d9f26d00ced531cf2ad18479280293
Darya_Kosik <dkosik@iba.by>
2024-10-27 21:04:58 +0300

    modal with react-router-dom

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,20 +1,29 @@
 import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppHeader } from '../app-header/app-header';
 import { HomePage } from '../../pages/home/home';
 import { NotFound404Page } from '../../pages/not-found-404/not-found-404';
 import { getIngredientsData } from '../../services/actions/ingredients';
+import { IngredientDetails } from '../ingredient-details/ingredient-details';
+import { Modal } from '../modal/modal';
 import styles from "./app.module.css"
-
 
 function App() {
   const dispatch = useDispatch();
   const { ingredientsRequest, ingredientsError, ingredients } = useSelector(state => state.ingredients);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const state = location.state;
 
   useEffect(() => {
     dispatch(getIngredientsData());
   }, []);
+
+  const onCloseModal = e => {
+    e.stopPropagation();
+    navigate("/");
+  };
 
   if (ingredientsRequest) {
     return <p className="text text_type_main-default text_color_inactive">
@@ -35,9 +44,21 @@ function App() {
   return (
     <div className={styles.page}>
       <AppHeader />
-      <Routes>
+      {state?.backgroundLocation && (
+        <Routes>
+          <Route
+            path="/ingredients/:id"
+            element={
+              <Modal title="Детали ингредиента" onClose={onCloseModal}>
+                <IngredientDetails />
+              </Modal>
+            } />
+        </Routes>
+      )}
+      <Routes location={state?.backgroundLocation || location}>
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={<NotFound404Page />} />
+        <Route path="/ingredients/:id" element={<IngredientDetails />} />
       </Routes>
     </div>
   )
--- src/components/ingredient-details/ingredient-details.jsx
+++ src/components/ingredient-details/ingredient-details.jsx
@@ -1,38 +1,43 @@
-import { IngredientItemType } from "./../../utils/types";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import styles from "./ingredient-details.module.css";
 
-function IngredientDetails({ item }) {
+function IngredientDetails() {
+    const { id } = useParams();
+    const { ingredients } = useSelector(state => state.ingredients);
+    const selectedIngredient = ingredients.find(item => item._id === id);
+
+    if (!selectedIngredient) {
+        return <p className="text text_type_main-default text_color_inactive">Ингредиент не найден</p>
+    }
+
     return (
         <div className={styles.container}>
-            <img src={item.image_large} alt={item.image_large} />
+            <img src={selectedIngredient?.image_large} alt={selectedIngredient?.image_large} />
             <p className="text text_type_main-medium pt-4 pb-8">
-                {item.name}
+                {selectedIngredient?.name}
             </p>
             <div className={`${styles.section} pb-15`}>
                 <div className={styles.composition}>
                     <p className="text text_type_main-default text_color_inactive">Калории, ккал</p>
-                    <p className="text text_type_digits-medium text_color_inactive">{item.proteins}</p>
+                    <p className="text text_type_digits-medium text_color_inactive">{selectedIngredient?.proteins}</p>
                 </div>
 
                 <div className={styles.composition}>
                     <p className="text text_type_main-default text_color_inactive">Белки, г</p>
-                    <p className="text text_type_digits-medium text_color_inactive">{item.fat}</p>
+                    <p className="text text_type_digits-medium text_color_inactive">{selectedIngredient?.fat}</p>
                 </div>
                 <div className={styles.composition}>
                     <p className="text text_type_main-default text_color_inactive">Жиры, г</p>
-                    <p className="text text_type_digits-medium text_color_inactive">{item.carbohydrates}</p>
+                    <p className="text text_type_digits-medium text_color_inactive">{selectedIngredient?.carbohydrates}</p>
                 </div>
                 <div className={styles.composition}>
                     <p className="text text_type_main-default text_color_inactive">Углеводы, г</p>
-                    <p className="text text_type_digits-medium text_color_inactive">{item.calories}</p>
+                    <p className="text text_type_digits-medium text_color_inactive">{selectedIngredient?.calories}</p>
                 </div>
             </div>
         </div>
     )
-}
-
-IngredientDetails.propTypes = {
-    item: IngredientItemType.isRequired
 }
 
 export { IngredientDetails };

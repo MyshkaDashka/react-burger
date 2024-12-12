a4ea78d2f7917d925c15d48ba04e4afd2f540b4e
Darya_Kosik <dkosik@iba.by>
2024-12-12 23:33:03 +0300

    for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import { configureStore } from './services/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/app/app';
 //@ts-ignore
 import './index.css';
@@ -12,9 +12,9 @@ const store = configureStore();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>,
 )
